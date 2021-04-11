ef70e89d12895623ccc115883cc3e9fed17f4cb0
rsundar <rohan.s.1987@gmail.com>
2021-04-11 22:28:50 +0530

    added arcade physics engine configuration options

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -22,8 +22,9 @@ class Game extends Phaser.Scene {
       repeat: -1,
     });
     
-    this.player = this.add.sprite(400, 300, 'hero-run-sheet');
+    this.player = this.physics.add.sprite(400, 300, 'hero-run-sheet');
     this.player.anims.play('hero-running');
+    this.player.body.setCollideWorldBounds(true);
   }
 
   update(time, delta) {}
